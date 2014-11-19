f754f20b94c1a9ef64701fa585eb41326e003f34
Michal Margiel <michal.margiel@gmail.com>
2014-11-19 20:40:38 +0100

    Initial commit

---
+++ src/main/java/pl/confitura/jelatyna/Application.java
@@ -0,0 +1,34 @@
+package pl.confitura.jelatyna;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@ComponentScan
+@EnableAutoConfiguration
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+    @Configuration
+    @EnableWebSecurity
+    static class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+
+
+        @Override
+        protected void configure(HttpSecurity http) throws Exception {
+            http.anonymous();
+        }
+
+
+    }
+}
