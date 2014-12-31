fe3cc934a4a1f86d154a0949f20d492853abe8bd
Jakub Kubrynski <jkubrynski@gmail.com>
2014-12-31 18:24:16 +0100

    Introduced Spock and updated SpringBoot to 1.2.0.RELEASE

--- src/main/java/pl/confitura/jelatyna/Application.java
+++ src/main/java/pl/confitura/jelatyna/Application.java
@@ -1,12 +1,8 @@
 package pl.confitura.jelatyna;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -21,8 +17,6 @@ public class Application {
     @Configuration
     @EnableWebSecurity
     static class WebSecurityConfig extends WebSecurityConfigurerAdapter {
-
-
 
         @Override
         protected void configure(HttpSecurity http) throws Exception {
