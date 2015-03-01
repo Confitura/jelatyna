885e85ee1a25909deefc949495e5f3934037a013
Michal Margiel <Michal.Margiel@gmail.com>
2015-03-01 19:03:05 +0100

    clean tests

--- src/test/groovy/pl/confitura/jelatyna/email/EmailServiceTest.groovy
+++ src/test/groovy/pl/confitura/jelatyna/email/EmailServiceTest.groovy
@@ -5,9 +5,10 @@ import org.springframework.boot.test.SpringApplicationContextLoader
 import org.springframework.test.context.ContextConfiguration
 import pl.confitura.jelatyna.Application
 import pl.confitura.jelatyna.admin.Admin
+import spock.lang.Ignore
 import spock.lang.Specification
 
-//@Ignore("This is not a unity test. It is used to check if the integration is working")
+@Ignore("This is not a unity test. It is used to check if the integration with mandril is working")
 @ContextConfiguration(loader = SpringApplicationContextLoader.class, classes = Application.class)
 class EmailServiceTest extends Specification {
     @Autowired
