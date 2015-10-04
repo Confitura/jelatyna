8c99133683f2be109b74060d0004da8f8baffdef
Dawid Nejman <dawidnejman@gmail.com>
2015-10-04 19:52:37 +0200

    Add static analysis tool for groovy
    There are many tests written in Groovy. We could benefit from static analysis tool for it, as Groovy by itself allows many different coding styles and conventions.

---
+++ codenarc.groovy
@@ -0,0 +1,22 @@
+ruleset {
+    ruleset('rulesets/basic.xml')
+    ruleset('rulesets/braces.xml')
+    ruleset('rulesets/concurrency.xml')
+    ruleset('rulesets/convention.xml')
+    ruleset('rulesets/design.xml')
+    ruleset('rulesets/dry.xml')
+    ruleset('rulesets/formatting.xml')
+    ruleset('rulesets/generic.xml')
+    ruleset('rulesets/groovyism.xml')
+    ruleset('rulesets/imports.xml')
+    ruleset('rulesets/jdbc.xml')
+    ruleset('rulesets/junit.xml')
+    ruleset('rulesets/logging.xml')
+    ruleset('rulesets/naming.xml')
+    ruleset('rulesets/security.xml')
+    ruleset('rulesets/size.xml') {
+        'CrapMetric' enabled: false
+    }
+    ruleset('rulesets/unnecessary.xml')
+    ruleset('rulesets/unused.xml')
+}
